2a1b5f55cdfd348444c8bb8a999e034a7bfcc611
ThiagoLaass <thiagolaass202@gmail.com>
2024-04-21 19:52:59 -0300

    Add abstract methods and fields to CrewMember class

--- Nasa.java
+++ Nasa.java
@@ -1,6 +1,4 @@
 public class Nasa {
 
-    public static void main(String[] args) {
-        
-    }
+    
 }
--- SpaceMission.java
+++ SpaceMission.java
@@ -1,4 +1,6 @@
 public class SpaceMission {
     
-    
+    public static void main(String[] args) {
+        
+    }
 }
